db3b8438cffbd6dbbd1ea39445fd65b0b3924f29
Sebastian Yang <yukaiy@andrew.cmu.edu>
2019-03-24 15:36:32 -0400

    More testings

--- Assets/Scripts/CameraFollowPlayer.cs
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraFollowPlayer : MonoBehaviour
 {
     [SerializeField] private Transform _target;
+    public float moveSpeed = 2f;
     public float smoothTIme = 0.2f;
     private Vector3 _velocity = Vector3.zero;
 
@@ -12,7 +13,7 @@ public class CameraFollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 targetPos = new Vector3(_target.position.x + 2f, _target.position.y, transform.position.z);
+        Vector3 targetPos = new Vector3((transform.position.x + moveSpeed*Time.deltaTime), _target.position.y, transform.position.z);
         transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref _velocity, smoothTIme);
     }
 }
